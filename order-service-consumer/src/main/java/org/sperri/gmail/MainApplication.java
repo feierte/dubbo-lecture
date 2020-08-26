e8a7c9deb8eb86574b11c9e85d87fa6ba5466308
zhaojie <1979170837@qq,com>
2020-08-26 21:30:46 +0800

    update

---
+++ order-service-consumer/src/main/java/org/sperri/gmail/MainApplication.java
@@ -0,0 +1,21 @@
+package org.sperri.gmail;
+
+import java.io.IOException;
+
+import org.sperri.gmail.service.OrderService;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class MainApplication {
+	
+	@SuppressWarnings("resource")
+	public static void main(String[] args) throws IOException {
+		ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext("consumer.xml");
+		
+		OrderService orderService = applicationContext.getBean(OrderService.class);
+		
+		orderService.initOrder("1");
+		System.out.println("调用完成....");
+		System.in.read();
+	}
+
+}
